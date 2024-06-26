d991645b58520ecb6886fc5aab35ab3656c18891
Dan Egnor <egnor@ofb.net>
2024-06-26 14:21:18 -0700

    updated updater

--- update_package.py
+++ update_package.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import re
 import subprocess
 import urllib
+import yaml
 import zipfile
 
 repo_dir = Path(__file__).parent
@@ -16,33 +17,44 @@ git_show = subprocess.check_output(
     text=True, cwd=index_path.parent
 )
 
-head_index = json.loads(git_show)
-tree_index = json.loads(index_path.read_text())
+git_index = json.loads(git_show)
+git_platform = git_index["packages"][0]["platforms"][0]
+git_version = git_platform["version"]
+version = re.sub(r"\d+$", lambda m: str(int(m.group()) + 1), git_version)
 
-head_platform = head_index["packages"][0]["platforms"][0]
-tree_platform = tree_index["packages"][0]["platforms"][0]
+index = json.loads(index_path.read_text())
+platform = index["packages"][0]["platforms"][0]
+zip_path = repo_dir / f"{core_name}-{version}.zip"
+old_url = urllib.parse.urlparse(platform["url"])
+url_path = re.sub(r"/[^/]*$", f"/{zip_path.name}", old_url.path)
+url = old_url._replace(path=url_path)
 
-head_version = head_platform["version"]
-tree_version = tree_platform["version"]
+project_path = repo_dir / "test_sketch" / "sketch.yaml"
+project = yaml.load(project_path.read_text(), Loader=yaml.Loader)
+project_profile = project["default_profile"]
+for project_platform in project["profiles"][project_profile]["platforms"]:
+    index_url = urllib.parse.urlparse(project_platform["platform_index_url"])
+    if index_url.path.endswith(f"/{index_path.name}"):
+        platform_ref = project_platform["platform"]
+        platform_ref = re.sub(r"\(\d+(\.\d+)+\)", f"({version})", platform_ref)
+        project_platform["platform"] = platform_ref
 
-for old_version in set((head_version, tree_version)):
-    old_path = repo_dir / f"{core_name}-{old_version}.zip"
-    old_path.unlink(missing_ok=True)
+old_version = platform["version"]
+for old_version in set((git_version, old_version)):
+    old_zip_path = repo_dir / f"{core_name}-{old_version}.zip"
+    old_zip_path.unlink(missing_ok=True)
 
-next_version = re.sub(r"\d+$", lambda m: str(int(m.group()) + 1), head_version)
-new_path = repo_dir / f"{core_name}-{next_version}.zip"
-
-# make zip file with all files from esp/
-with zipfile.ZipFile(new_path, "w") as zf:
-    for file in repo_dir.glob("esp/**/*"):
+with zipfile.ZipFile(zip_path, "w") as zf:
+    for file in repo_dir.glob("esp32/**/*"):
         zf.write(file, file.relative_to(repo_dir))
 
-tree_platform["version"] = next_version
-tree_platform["url"] = tree_platform["url"].replace(tree_version, next_version)
-tree_platform["archiveFileName"] = new_path.name
+zip_bytes = zip_path.read_bytes()
 
-zip_bytes = new_path.read_bytes()
-tree_platform["size"] = len(zip_bytes)
-tree_platform["checksum"] = "SHA-256:" + hashlib.sha256(zip_bytes).hexdigest()
+platform["version"] = version
+platform["url"] = url.geturl()
+platform["archiveFileName"] = zip_path.name
+platform["size"] = len(zip_bytes)
+platform["checksum"] = "SHA-256:" + hashlib.sha256(zip_bytes).hexdigest()
 
-index_path.write_text(json.dumps(tree_index, indent=2) + "\n")
+project_path.write_text(yaml.dump(project))
+index_path.write_text(json.dumps(index, indent=2) + "\n")
